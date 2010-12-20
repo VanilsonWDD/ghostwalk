e45220945c7ce85bb9247648513459db6455cb39
José Mota <josemota.net@gmail.com>
2010-12-20 21:06:47 +0000

    add plugin file.

---
+++ ghostwalk.js
@@ -0,0 +1,15 @@
+jQuery.fn.ghostwalk = function(options) {
+  var defaults = {
+    speed   : 300
+  , timeout : 3000
+  };
+  var options = jQuery.extend(defaults,options);
+	return this.each(function() {
+    obj = jQuery(this);
+		obj.hide().slideDown(options.speed);
+    setTimeout(function() {
+      obj.slideUp(options.speed);
+      setTimeout(function() {obj.remove(); }, options.speed);
+    },options.timeout);
+	});
+}
